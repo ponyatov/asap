935efd00429bb2027a7bae32a8ce6215fb530098
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-06 15:08:02 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/l496disco/Core/Inc/main.h
+++ hw/l496disco/Core/Inc/main.h
@@ -77,8 +77,10 @@ void Error_Handler(void);
 #define SWCLK_GPIO_Port GPIOA
 #define SWDIO_Pin LL_GPIO_PIN_13
 #define SWDIO_GPIO_Port GPIOA
-#define LED1_Pin LL_GPIO_PIN_13
-#define LED1_GPIO_Port GPIOB
+#define LD3_Pin LL_GPIO_PIN_5
+#define LD3_GPIO_Port GPIOA
+#define LD2_Pin LL_GPIO_PIN_13
+#define LD2_GPIO_Port GPIOB
 #ifndef NVIC_PRIORITYGROUP_0
 #define NVIC_PRIORITYGROUP_0         ((uint32_t)0x00000007) /*!< 0 bit  for pre-emption priority,
                                                                  4 bits for subpriority */
