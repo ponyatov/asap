fee49af78cf64f14e673172c887e054fa67041f5
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-06 14:57:06 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/l496disco/Core/Src/gpio.c
+++ hw/l496disco/Core/Src/gpio.c
@@ -49,15 +49,15 @@ void MX_GPIO_Init(void)
   LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
 
   /**/
-  LL_GPIO_ResetOutputPin(LED_GPIO_Port, LED_Pin);
+  LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin);
 
   /**/
-  GPIO_InitStruct.Pin = LED_Pin;
+  GPIO_InitStruct.Pin = LED1_Pin;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  LL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+  LL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
 
 }
 
